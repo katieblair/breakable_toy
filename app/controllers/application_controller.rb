6c47e9ebea5834368c10ca32fb300d021fa008cf
Katie Blair <katieblair89@gmail.com>
2014-07-10 11:05:47 -0400

    Called authorized to edit as helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  helper_method :authorized_to_edit?
 
   protected
 
