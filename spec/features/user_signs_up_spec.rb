fec1ff75235622d3c8407e070e1bfda7e92e7683
Katie Blair <katieblair89@gmail.com>
2014-07-10 12:55:18 -0400

    Added user signs up spec

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -23,4 +23,68 @@ feature 'user signs up', %Q(
 # I should get a success message if it is posted successfully.
 # I should get an error message if it is not posted successfully.
 
+  scenario 'user signs up' do
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Username', with: user.username
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password
+    click_button 'Sign Up'
+
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign Up')
+    expect(page).to_not have_content('Sign In')
+  end
+
+  scenario 'user signs up with non-matching passwords' do
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Username', with: user.username
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: 'wrong'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("confirmation doesn't match")
+
+  end
+
+  scenario 'user fails to enter required information' do
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password
+    click_button 'Sign Up'
+
+    expect(page).to have_content("can't be blank") #How to do this multiple times?
+  end
+
+  scenario 'user signs up with profile photo' do #Does this need to be its own test?
+    user = FactoryGirl.build(:user)
+
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'First Name', with: user.first_name
+    fill_in 'Last Name', with: user.last_name
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password Confirmation', with: user.password
+    attach_file 'Profile Photo', File.join(Rails.root, '/spec/support/example.jpg')
+    click_button 'Sign Up'
+
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign Up')
+  end
 end
