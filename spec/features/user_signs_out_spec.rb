ed40466030546e0c97cddae38649c32ce91380cf
Katie Blair <katieblair89@gmail.com>
2014-07-10 13:10:30 -0400

    Added user signs out spec

--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -12,4 +12,14 @@ feature 'user signs out', %Q(
 # I should get a success message if I am signed out successfully.
 # I should get an error message if I am not signed out successfully.
 
+  scenario 'user signs out' do
+    user = FactoryGirl.create(:user)
+
+    visit '/'
+    click_on 'Sign Out'
+
+    expect(page).to have_content('Sign Up')
+    expect(page).to have_content('Sign In')
+    expect(page).to_not have_content('Sign Out')
+  end
 end
