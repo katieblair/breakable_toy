063c750d4b8cd78bfe8b44401fd8f641e6411f35
Katie Blair <katieblair89@gmail.com>
2014-07-11 12:09:06 -0400

    Listed seeds--not seeded yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# genres = Genre.create([{ name: 'Poetry' }, { name: 'Essay' }, { name: 'Short Story' }, { name: 'Literary Fiction' }, { name: 'Historical Fiction' }, { name: 'Science Fiction' }, { name: 'Fantasy' }, { name: 'Horror' }, { name: 'Mystery/Thriller' }, { name: 'Romance' }, { name: 'Nonfiction/Memoir' }, { name: "Children's" }, { name: 'Middle Grade' }, { name: 'Young Adult' }, { name: 'Other' }])
