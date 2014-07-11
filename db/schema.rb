a2fd0fab366a0a76aff07d17d6b2344c21b6861a
Katie Blair <katieblair89@gmail.com>
2014-07-11 11:36:34 -0400

    Created genres migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140710200254) do
+ActiveRecord::Schema.define(version: 20140711153538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,10 @@ ActiveRecord::Schema.define(version: 20140710200254) do
     t.text     "body",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "genres", force: true do |t|
+    t.string "name", null: false
   end
 
   create_table "users", force: true do |t|
