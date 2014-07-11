815d6596090d19c98e079bdbb4f9c6f6096f053e
Katie Blair <katieblair89@gmail.com>
2014-07-11 13:33:47 -0400

    Added genre routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :critiques
   end
   resources :critiques, only: [:edit, :update, :destroy]
+  resources :genres, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
