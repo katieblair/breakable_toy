c24ed9ce60651b8aa4d754037405b45053dbc28e
Katie Blair <katieblair89@gmail.com>
2014-07-12 20:27:08 -0400

    Added groups table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140711153538) do
+ActiveRecord::Schema.define(version: 20140713001818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,11 @@ ActiveRecord::Schema.define(version: 20140711153538) do
 
   create_table "genres", force: true do |t|
     t.string "name", null: false
+  end
+
+  create_table "groups", force: true do |t|
+    t.string "name",        null: false
+    t.text   "description"
   end
 
   create_table "users", force: true do |t|
