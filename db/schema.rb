f81e26d6cdd22a2303cebbee0b0fd78ae3b0f6de
Katie Blair <katieblair89@gmail.com>
2014-07-12 20:29:23 -0400

    Created memberships table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713001818) do
+ActiveRecord::Schema.define(version: 20140713002810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,11 @@ ActiveRecord::Schema.define(version: 20140713001818) do
   create_table "groups", force: true do |t|
     t.string "name",        null: false
     t.text   "description"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer "user_id",  null: false
+    t.integer "group_id", null: false
   end
 
   create_table "users", force: true do |t|
