f4b63f9306ff7e9482cdfe75ba709cbde75c1cb9
Katie Blair <katieblair89@gmail.com>
2014-07-14 10:15:53 -0400

    Added new and create actions and group params method

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,4 +8,25 @@ class GroupsController < ApplicationController
   def show
     @group = Group.find(params[:id])
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      flash[:notice] = 'Success!'
+      redirect_to groups_path
+    else
+      flash[:notice] = 'Error'
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :description)
+  end
 end
