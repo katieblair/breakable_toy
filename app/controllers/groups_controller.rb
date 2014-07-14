f804d17833c5cd44b0bbaf7267dd7141923e6bb8
Katie Blair <katieblair89@gmail.com>
2014-07-14 10:49:44 -0400

    Added moderator functions to groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,6 +16,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    @group.user_id = current_user.id
     if @group.save
       flash[:notice] = 'Success!'
       redirect_to groups_path
