a4720a3e1adf44da22f01ad93db28876d31d9f9f
Katie Blair <katieblair89@gmail.com>
2014-07-14 18:18:53 -0400

    Update user bio feature is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,24 @@ class UsersController < ApplicationController
     @memberships = Membership.where(user: @user)
     @drafts = Draft.where(user: @user)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'Success!'
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:bio)
+  end
 end
