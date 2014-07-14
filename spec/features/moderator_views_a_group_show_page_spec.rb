9a20de500fc81b1a42efcc25ef3da204f2edf6f2
Katie Blair <katieblair89@gmail.com>
2014-07-14 13:30:18 -0400

    Added user story to spec

---
+++ spec/features/moderator_views_a_group_show_page_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'moderator views a group show page', %Q(
+  As a group moderator
+  I want to view my group's page
+  So that I can edit its information and members
+) do
+
+end
