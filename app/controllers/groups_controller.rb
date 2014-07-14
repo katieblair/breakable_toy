d5a88d636af7a388cc2fa013fc8d425e9d803050
Katie Blair <katieblair89@gmail.com>
2014-07-14 10:06:48 -0400

    Created basic index and show actions for groups

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,11 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+end
