116ea7561f20a899995fd9e10f830b1241a2560f
Katie Blair <katieblair89@gmail.com>
2014-07-14 10:52:23 -0400

    Added user id for moderator to groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713002810) do
+ActiveRecord::Schema.define(version: 20140714145109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20140713002810) do
   end
 
   create_table "groups", force: true do |t|
-    t.string "name",        null: false
-    t.text   "description"
+    t.string  "name",        null: false
+    t.text    "description"
+    t.integer "user_id"
   end
 
   create_table "memberships", force: true do |t|
