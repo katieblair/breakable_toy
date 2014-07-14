6372d5eb9c06f2ca98122dffcbd9b1696dedd39b
Katie Blair <katieblair89@gmail.com>
2014-07-14 16:23:50 -0400

    Fixed group and moderator association; moderator username displaying correctly

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveRecord::Base
   validates :name, presence: true
-  belongs_to :moderator, class_name: "User"#, foreign_key: 'user_id'
+  belongs_to :moderator, class_name: "User", foreign_key: 'user_id'
 
   has_many :memberships
   has_many :members, through: :memberships, class_name: "User"
